4a903d8a73b9538a28bd8b0edaeca2f36717e964
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-24 16:17:26 +0100

    Implemented number 171

--- Excel_col_num.py
+++ Excel_col_num.py
@@ -2,4 +2,10 @@
 
 
 def title_to_number(column_title: str) -> int:
-    return 0
+    col_num = 0
+    for i in range(len(column_title)):
+        idx = len(column_title) - (i+1)
+        col_num += (ord(column_title[idx]) - 64) * (26**i)
+
+    return col_num
+
