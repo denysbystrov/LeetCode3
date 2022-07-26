d59b45fe22bf09d1263a150d94d8cbef102fb0c9
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-26 09:40:41 +0100

    Implemented tests for number 190

---
+++ reverse_bits.py
@@ -0,0 +1,5 @@
+"""Number 190: Reverse Bits"""
+
+
+def reverse_bits(n: int) -> int:
+    return 0
---
+++ test_reverse_bits.py
@@ -0,0 +1,15 @@
+import pytest
+from reverse_bits import reverse_bits
+
+
+test_cases = (
+    (43261596, 964176192),
+    (4294967293, 3221225471),
+    (2147483647, 2147483647),
+    (1, 2147483648)
+)
+
+
+@pytest.mark.parametrize(('input_x', 'expected'), test_cases)
+def test_reverse_bits(input_x, expected):
+    assert reverse_bits(input_x) == expected
