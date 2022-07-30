9f60c89b0ed0de18a2bd2858d05a824650f407c9
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-30 15:11:29 +0100

    Implemented number 202

--- happy_num.py
+++ happy_num.py
@@ -2,4 +2,17 @@
 
 
 def is_happy(n: int) -> bool:
+    memo = set()
+
+    while n not in memo:
+        if n == 1:
+            return True
+        memo.update([n])
+        result = 0
+        while n:
+            result += (n % 10) ** 2
+            n = n // 10
+
+        n = result
+
     return False
