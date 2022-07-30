0be590ed6fd0078ebcabb95608f9909f826622a1
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-07-30 15:00:25 +0100

    Implemented number 191 neetcode way

--- hamming_weight.py
+++ hamming_weight.py
@@ -3,9 +3,9 @@
 
 def hamming_weight(n: int) -> int:
     count = 0
-    while n > 0:
-        count += n & 1
-        n = n >> 1
+    while n:
+        n = n & (n-1)
+        count += 1
 
     return count
 
