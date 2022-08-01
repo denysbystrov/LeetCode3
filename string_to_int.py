5d304b0f8f786ec3b4f36b3214cd26e95513d99c
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-01 18:53:57 +0100

    Implemented number 8 google engineer way

--- string_to_int.py
+++ string_to_int.py
@@ -2,29 +2,32 @@
 
 
 def my_atoi(s: str) -> int:
-    result = 0
-    negative = False
-    num_started = False
-    for i in range(len(s)):
-        if s[i] == '-' or s[i] == "+":
-            if num_started:
-                break
-            num_started = True
-            negative = (s[i] == '-')
-        elif s[i] == " " and not num_started:
-            continue
-        elif 48 <= ord(s[i]) <= 57:
-            num_started = True
-            d = int(s[i])
-            if result > 214748364:
-                result = 2147483648 if negative else 2147483647
-            elif result == 214748364:
-                max_digit = 8 if negative else 7
-                result = result*10 + max_digit if d > max_digit else result*10 + d
-            else:
-                result = result*10 + d
-        else:
-            break
+    i = 0
+    res = 0
+    negative = 1
 
-    return result if not negative else -result
+    while i < len(s) and s[i] == " ":
+        i += 1
+
+    if i < len(s) and s[i] == "+":
+        i += 1
+    elif i < len(s) and s[i] == "-":
+        negative = -1
+        i += 1
+
+    integers = set('0123456789')
+    while i < len(s) and s[i] in integers:
+        d = int(s[i])
+        if res > 214748364:
+            return -2147483648 if negative == -1 else 2147483647
+        elif res == 214748364:
+            max_digit = 8 if negative == -1 else 7
+            res = res * 10 + max_digit if d > max_digit else res * 10 + d
+        else:
+            res = res * 10 + d
+        i += 1
+
+    return res*negative
+
+
 
