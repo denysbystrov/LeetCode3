87b147d9ef14075e730b449c494cae0f24f0658d
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-01 18:34:24 +0100

    Implemented number 8

--- string_to_int.py
+++ string_to_int.py
@@ -4,23 +4,27 @@
 def my_atoi(s: str) -> int:
     result = 0
     negative = False
-    tens_count = 0
+    num_started = False
     for i in range(len(s)):
-        if s[i] == '-':
-            negative = True
-        elif 48 <= ord(s[i]) <= 57:
-            d = int(s[i])
-            if tens_count == 9 and result == 214748364:
-                if negative:
-                    result = 2147483648 if int(d) > 8 else result * 10 + d
-                else:
-                    result = 2147483647 if int(d) > 7 else result * 10 + d
-            else:
-                result = result * 10 + d
-                tens_count += 1
-        else:
-            if result > 0:
+        if s[i] == '-' or s[i] == "+":
+            if num_started:
                 break
+            num_started = True
+            negative = (s[i] == '-')
+        elif s[i] == " " and not num_started:
+            continue
+        elif 48 <= ord(s[i]) <= 57:
+            num_started = True
+            d = int(s[i])
+            if result > 214748364:
+                result = 2147483648 if negative else 2147483647
+            elif result == 214748364:
+                max_digit = 8 if negative else 7
+                result = result*10 + max_digit if d > max_digit else result*10 + d
+            else:
+                result = result*10 + d
+        else:
+            break
 
     return result if not negative else -result
 
