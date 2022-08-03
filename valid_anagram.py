58d90edde8fc064c3794b581a0789bf12f64e5ce
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-03 19:34:26 +0100

    Implemented number 242

--- valid_anagram.py
+++ valid_anagram.py
@@ -2,4 +2,22 @@
 
 
 def is_anagram(s: str, t: str) -> bool:
-    return False
+    if len(s) != len(t):
+        return False
+
+    s_dict = {}
+    for i in range(len(s)):
+        if s[i] in s_dict:
+            s_dict[s[i]] += 1
+        else:
+            s_dict[s[i]] = 1
+
+    for j in range(len(t)):
+        if t[j] not in s_dict:
+            return False
+        if s_dict[t[j]] == 0:
+            return False
+        s_dict[t[j]] -= 1
+
+    return True
+
