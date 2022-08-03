68da3488a9f67889bdfac805b3a12a319920d8de
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-03 14:19:14 +0100

    Implemented number 237

--- del_node.py
+++ del_node.py
@@ -2,5 +2,10 @@
 from Util import ListNode
 
 
-def delete_node(node) -> None:
-    return None
+def delete_node(node: ListNode) -> None:
+    while node.next.next:
+        node.val = node.next.val
+        node = node.next
+
+    node.val = node.next.val
+    node.next = None
