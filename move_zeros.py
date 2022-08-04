bdda3f07e6ecf00849ed8359d53c2583a85a0ecd
denysb23 <75504352+denysb23@users.noreply.github.com>
2022-08-04 16:43:07 +0100

    Implemented number 283

--- move_zeros.py
+++ move_zeros.py
@@ -3,4 +3,16 @@ from typing import List
 
 
 def move_zeroes(nums: List[int]) -> None:
-    return None
+    zero_p = 0
+    current_p = 0
+
+    while current_p < len(nums):
+        if nums[current_p]:
+            if nums[zero_p] == 0:
+                nums[zero_p] = nums[current_p]
+                nums[current_p] = 0
+                zero_p += 1
+            else:
+                zero_p += 1
+        current_p += 1
+
